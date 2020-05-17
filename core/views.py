638a207ddef21da6b20bf48062dee867a971ff2c
Abigail <castanedalunaabigail@gmail.com>
2020-05-17 14:56:45 -0500

    se crearon las paginas de porftafolio y contacto, se realizo la introdución a las plantillas

--- core/views.py
+++ core/views.py
@@ -6,17 +6,28 @@ html_base = """
 <ul>
    <li><a href="/">Portada</a></li>
    <li><a href="/about-me">Acerca de</a></li>
+   <li><a href="/portfolio">Portafolio</a></li>
+   <li><a href="/contact">Contacto</a></li>
 </ul>
    """
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p> Esto es la poartada</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
    return HttpResponse(html_base + """
         <h2>Acerca de </h2>
         <p> Me llamo abby y estudio TI</p>
+    """)
+
+def portfolio(request):
+    return HttpResponse(html_base + """
+        <h2>Portafolio</h2>
+        <p> Algunos de mis trabajos </p>
+    """)
+
+def contact(request):
+   return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aqui les dejo mi email para preguntarme dudas: <a href="mailito:castanedalunaabigail@gmail.com">castanedalunaabigail@gmail.com</a></p>
     """)
